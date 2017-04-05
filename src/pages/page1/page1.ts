d4537cc649118ad61b1827151f761b8595c0a8b4
Danilo Torquato <danilo.torquato@indexdigital.com.br>
2017-04-05 10:58:52 -0300

    no message

--- src/pages/page1/page1.ts
+++ src/pages/page1/page1.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { Details } from '../details/details';
+import { LoadingController } from 'ionic-angular';
 import {HttpProvider} from '../../providers/http-provider';
 
 
@@ -13,25 +14,32 @@ export class Page1 {
   items: any;
 
 
-  constructor(public navCtrl: NavController, private httpProvider :HttpProvider){
-
+  constructor(public navCtrl: NavController, private httpProvider :HttpProvider, private loadingController: LoadingController){
     this.getdata();
   }
 
   getdata(){
+    let loader = this.loadingController.create({
+        content: "Aguarde"
+      });
+  loader.present();
 
   this.httpProvider.getJsonData().subscribe(
     result => {
-      this.items=result;
+      this.items = result;
       console.log("Success : "+this.items);
+
     },
     err =>{
       console.error("Error : "+err);
+
     } ,
     () => {
       console.log('getData completed');
+        loader.dismiss()
     }
   );
+
 }
 
 
